5c88406e6e1d6943b5a7169fa3705e44620fd79d
howardVoxcan <vqnhoang97tn@gmail.com>
2026-02-09 15:19:11 +0700

    SWR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,31 @@ import x from '../styles/app.module.css'
 import y from '../styles/hoang.module.css'
 import BasicExample from '@/components/app.table'
 import { useEffect } from 'react';
+import useSWR from 'swr';
 
 export default function Home() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('http://localhost:8000/blogs');
-      const data = await res.json();
-      console.log(data);
-    }
-    fetchData();
-  }, []);
+  const fetcher = (url: string) => fetch(url)
+  .then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/blogs",
+    fetcher
+  );
+
+  console.log({data, error, isLoading});
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch('http://localhost:8000/blogs');
+  //     const data = await res.json();
+  //     console.log(data);
+  //   }
+  //   fetchData();
+  // }, []);
 
   return (
     <div>
+      {data?.length}
       <ul>
         <li className = {x.Facebook}>
           <Link href="facebook">Facebook Page</Link>
