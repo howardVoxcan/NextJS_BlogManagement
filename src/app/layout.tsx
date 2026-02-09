e5550eb532b2bbfd8fe8b6fd4b23e0cfc684e9f1
howardVoxcan <vqnhoang97tn@gmail.com>
2026-02-09 11:53:50 +0700

    Layout creation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>This is the layout above children</div>
+        {children}
+        <div>This is the layout below children</div>      
+      </body>
     </html>
   )
 }
