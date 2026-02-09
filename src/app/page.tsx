4d131ca484618c8f1a8c6c46c9a888fa75c5c3c4
howardVoxcan <vqnhoang97tn@gmail.com>
2026-02-09 15:00:10 +0700

    Fetching data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
 import x from '../styles/app.module.css'
 import y from '../styles/hoang.module.css'
 import BasicExample from '@/components/app.table'
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('http://localhost:8000/blogs');
+      const data = await res.json();
+      console.log(data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div>
       <ul>
